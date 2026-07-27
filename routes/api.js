5fa441d750b375ea5b01552399ea74e4c38d486c
Hugo140804 <hgugi3nsn@gmail.com>
2026-07-27 09:42:48 +0700

    Router api.js

--- routes/api.js
+++ routes/api.js
@@ -3,3 +3,9 @@ const router = express.Router();
 const { Komik } = require('../models');
 const komikController = require('../controller/komikcontroller'); // <-- TAMBAHKAN BARIS INI!
 
+router.get('/komik', komikController.getAllKomik);
+router.get('/komik/:id', komikController.getKomikById);
+router.post('/komik', komikController.createKomik);
+router.put('/komik/:id', komikController.updateKomik);
+router.delete('/komik/:id', komikController.deleteKomik);
+
