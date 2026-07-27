21761c8db3240b3bec6bc2aab090fab43d91278e
Hugo140804 <hgugi3nsn@gmail.com>
2026-07-27 09:40:30 +0700

    komikcontroller.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDatabase = require('./config/db');
+const connectDatabase = require('./config/db.js');
 
 const app = express();
 const PORT = 3000;
